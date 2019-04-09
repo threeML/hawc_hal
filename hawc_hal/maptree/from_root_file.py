36cb11a4b0e42dd04dd24172089f8ece83ce7de2
Chad <cbrisboi@mtu.edu>
2019-04-09 10:15:44 -0400

    logic more obvious with extra try statement and useful error message

--- hawc_hal/maptree/from_root_file.py
+++ hawc_hal/maptree/from_root_file.py
@@ -70,10 +70,12 @@ def from_root_file(map_tree_file, roi):
         try:
             data_bins_labels = list(root_numpy.tree2array(f.Get("BinInfo"), "name"))
         except ValueError:
-            data_bins_labels = list(root_numpy.tree2array(f.Get("BinInfo"), "id"))
-            data_bins_labels = [ str(i) for i in data_bins_labels ]
-        else:
-            raise ValueError, "Unknown Tree name: Not 'id' or 'name' "
+            try:
+                data_bins_labels = list(root_numpy.tree2array(f.Get("BinInfo"), "id"))
+                data_bins_labels = [ str(i) for i in data_bins_labels ]
+            except ValueError:
+                raise ValueError, "Unknown Tree name in maptree: Not 'id' or 'name' "
+
         # A transit is defined as 1 day, and totalDuration is in hours
         # Get the number of transit from bin 0 (as LiFF does)
 
