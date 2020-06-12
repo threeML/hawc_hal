87c37ba89344a31b50b65d4baebdb117b72abd76
ndilalla <niccolo.dilalla@stanford.edu>
2020-06-12 15:45:00 -0700

    Adding py37 to travis.

--- ci/install_miniconda.sh
+++ ci/install_miniconda.sh
@@ -16,21 +16,22 @@ if [ ! -d "$HOME/miniconda/envs/test-environment" ]; then
 
     if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 
     else
 
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
 
     fi
 
     # Install miniconda and all the packages
 
     bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
     hash -r
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
-    conda create -q -n test-environment -c conda-forge -c threeml python=2.7 root5 numba numpy scipy astropy healpy threeml
+    conda create -q -n test-environment -c conda-forge -c threeml python=$TRAVIS_PYTHON_VERSION root5 numba numpy scipy astropy healpy astromodels threeml
 
     set +x
 
