c0652136ce0e68a5efdd57cb6e27412ef49cad26
ndilalla <niccolo.dilalla@stanford.edu>
2020-06-24 19:28:38 -0700

    Minor.

--- ci/install_package.sh
+++ ci/install_package.sh
@@ -14,6 +14,8 @@ conda install -c conda-forge pytest-cov codecov cython
 #pip install root_numpy
 if [[ ${TRAVIS_PYTHON_VERSION} == 3.7 ]]; then
     pip install "astropy==3.2" "pytest==3.6"
+elif [[ "$OSTYPE" == darwin* ]]; then
+    conda update -c conda-forge -c threeml threeml astromodels
 fi
 pip install .
 
