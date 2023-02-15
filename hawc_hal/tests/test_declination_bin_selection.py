88e89767a70eca15d7a2c249bc0d47fb3458ede1
rishibbdb <rbabu@mtu.edu>
2023-02-15 04:11:26 -0500

    Beta V5: Tested and Working declination band scripts

--- hawc_hal/tests/test_declination_bin_selection.py
+++ hawc_hal/tests/test_declination_bin_selection.py
@@ -1,5 +1,8 @@
 import os
-
+from hawc_hal import HAL, HealpixConeROI
+from threeML import *
+import astromodels
+import astropy.units as u
 import pytest
 from hawc_hal import HealpixConeROI
 from hawc_hal.maptree.map_tree import map_tree_factory
@@ -8,6 +11,8 @@ from hawc_hal.region_of_interest import (
     HealpixMapROI
 )
 from hawc_hal.dec_band_select import dec_index_search
+test_data_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+response=os.path.join(test_data_path, "geminga_response.root")
 ra_source, dec_source = 101.7, 16
 data_r = 3
 roi = HealpixConeROI(data_radius=data_r,
@@ -15,7 +20,7 @@ roi = HealpixConeROI(data_radius=data_r,
                              ra=ra_source, dec=dec_source)
 
 dec_arange=[roi.ra_dec_center[1]-roi.model_radius.to(u.deg).value, roi.ra_dec_center[1]+roi.model_radius.to(u.deg).value]
-
+response_file=response
 #Use the declination band selection module
 dec_list = dec_index_search(response_file, dec_arange, use_module=True)
 print("Using declination list=", dec_list)
