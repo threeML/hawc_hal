3077d758dd91c7ceac4ed40a28d2fde85151d7a3
Ramiro Torres-Escobedo <rtorrese@proton.me>
2026-05-11 16:39:18 +0800

    handles new version of hdf5 files

--- hawc_hal/serialize.py
+++ hawc_hal/serialize.py
@@ -1,12 +1,13 @@
+import pickle
 from builtins import object
+
 from pandas import HDFStore
 
 
 # This object is to decouple the serialization from any particular implementation. At the moment we use HDF5 through
 # pandas but this might change in the future. Without changing the external API, only changes here will be necessary.
 class Serialization(object):
-
-    def __init__(self, filename, mode='r', compress=True):
+    def __init__(self, filename, mode="r", compress=True):
 
         self._filename = filename
         self._compress = compress
@@ -15,11 +16,11 @@ class Serialization(object):
     def __enter__(self):
 
         if self._compress:
-
-            self._store = HDFStore(self._filename, complib='blosc:lz4', complevel=9, mode=self._mode)
+            self._store = HDFStore(
+                self._filename, complib="blosc:lz4", complevel=9, mode=self._mode
+            )
 
         else:  # pragma: no cover
-
             self._store = HDFStore(self._filename, mode=self._mode)
 
         return self
@@ -35,13 +36,13 @@ class Serialization(object):
 
     def store_pandas_object(self, path, obj, **metadata):
 
-        self._store.put(path, obj, format='fixed')
+        self._store.put(path, obj, format="fixed")
 
-        self._store.get_storer(path).attrs.metadata = metadata
+        self._store.get_storer(path).attrs.metadata = pickle.dumps(metadata)
 
     def retrieve_pandas_object(self, path):
 
-        # Get the metadata
+        # Get the metadata (PyTables auto-unpickles stored pickle bytes)
         metadata = self._store.get_storer(path).attrs.metadata
 
         # Get the object
